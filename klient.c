9ad347ab5f4f4481fce6ab188437ef3b7a518661
Alicja Kowalska <alakowalska2000@wp.pl>
2025-01-19 22:35:26 +0100

    Zamiana wątków na procesy
    Zmiana nad którą pracowałam kilka dni - program pozbawiony wcześniejszych funkcjonalności jak na razie.

--- klient.c
+++ klient.c
@@ -1,47 +1,31 @@
 #include "supermarket.h"
 
-extern Kasa kasy[MAX_KASY];
-extern pthread_mutex_t liczba_klientow_mutex;
-extern int liczba_klientow;
-extern int liczba_czynnych_kas; 
+int main() {
+    key_t key = ftok("supermarket", 65);
+    int msgid = msgget(key, 0666 | IPC_CREAT);
 
-void *klient_zachowanie(void *arg) {
-    Klient *klient = (Klient *)arg;
+    srand(time(NULL));
+    int id_klienta = 0;
 
-    pthread_mutex_lock(&liczba_klientow_mutex);
-    liczba_klientow++;
-    pthread_mutex_unlock(&liczba_klientow_mutex);
+    while (1) {
+        Komunikat komunikat;
+        komunikat.mtype = MSG_TYPE_KLIENT;
+        komunikat.liczba_klientow = 1;
+        komunikat.wydatki = rand() % 1000 + 1;
 
-    printf("Klient %d: Wchodzę do supermarketu.\n", klient->id_klienta);
-    sleep(klient->czas_w_sklepie);
+        msgsnd(msgid, &komunikat, sizeof(Komunikat) - sizeof(long), 0);
 
-    int wybrana_kasa = -1, min_klientow = MAX_KLIENCI;
-    for (int i = 0; i < liczba_czynnych_kas; i++) {
-        pthread_mutex_lock(&kasy[i].mutex);
-        if (kasy[i].liczba_klientow < min_klientow) {
-            min_klientow = kasy[i].liczba_klientow;
-            wybrana_kasa = i;
-        }
-        pthread_mutex_unlock(&kasy[i].mutex);
+        printf("Klient %d: Wchodzę do supermarketu.\n", ++id_klienta);
+
+        sleep(rand() % 10 + 1);        // Symulacja czasu zakupów
+
+        komunikat.liczba_klientow = -1; 
+        msgsnd(msgid, &komunikat, sizeof(Komunikat) - sizeof(long), 0);
+
+        printf("Klient %d: Opuszczam supermarket.\n", id_klienta);
+        
+        usleep(rand() % 500000);
     }
 
-    pthread_mutex_lock(&kasy[wybrana_kasa].mutex);
-    kasy[wybrana_kasa].liczba_klientow++;
-    pthread_mutex_unlock(&kasy[wybrana_kasa].mutex);
-
-    printf("Klient %d: Czekam w kolejce do kasy %d.\n", klient->id_klienta, wybrana_kasa + 1);
-    sleep(CZAS_OBSLUGI);
-
-    pthread_mutex_lock(&kasy[wybrana_kasa].mutex);
-    kasy[wybrana_kasa].liczba_klientow--;
-    kasy[wybrana_kasa].zarobek += klient->wydatki;
-    pthread_mutex_unlock(&kasy[wybrana_kasa].mutex);
-
-    pthread_mutex_lock(&liczba_klientow_mutex);
-    liczba_klientow--;
-    pthread_mutex_unlock(&liczba_klientow_mutex);
-
-    printf("Klient %d: Zostałem obsłużony i wychodzę.\n", klient->id_klienta);
-    free(klient);
-    return NULL;
+    return 0;
 }
