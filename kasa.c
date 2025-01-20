31a6dae925819ed269c00fa242b442e4b23d3890
Alicja Kowalska <alakowalska2000@wp.pl>
2025-01-20 15:48:12 +0100

    Create kasa.c
    Utworzenie pliku kasa do obsługi kas.

---
+++ kasa.c
@@ -0,0 +1,28 @@
+#include "supermarket.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Podaj numer kasy jako argument\n");
+        exit(1);
+    }
+
+    int id = atoi(argv[1]);
+
+    // Połączenie z pamięcią współdzieloną
+    int shm_id = shmget(SHM_KEY, MAX_KASY * sizeof(Kasa), 0666);
+    if (shm_id < 0) {
+        perror("Nie udało się połączyć z pamięcią współdzieloną");
+        exit(1);
+    }
+
+    Kasa *kasy = (Kasa *)shmat(shm_id, NULL, 0);
+
+    while (1) {
+        if (kasy[id].czynna) {
+            printf("Kasa %d: Kolejka = %d\n", id + 1, kasy[id].kolejka);
+        }
+        sleep(1);
+    }
+
+    return 0;
+}
